999b3f28a3474417aa100dcbb69c037241cc8915
esther-must <esther.must92@gmail.com>
2025-02-19 11:41:42 +0100

    Props drilling

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer style={{ textAlign: 'center', backgroundColor: 'gray', color: 'white', padding: '10px' }}>
+            <p>© 2023 City Lovers</p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+    return (
+        <main style={{ padding: '20px', backgroundColor: '#f4f4f4' }}>
+            <p>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;
