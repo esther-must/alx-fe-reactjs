aac6f08c2e417cae10b6c78fb1b74d67e80c8d31
esther-must <esther.must92@gmail.com>
2025-02-19 14:44:38 +0100

    Fixed UserProfile

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -2,7 +2,7 @@ import ProfilePage from "./components/ProfilePage";
 import UserContext from "./components/UserContext";
 
 function App() {
-  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+  const userData = { name: "Jane Doe", age: 30, bio: "A passionate software developer", email: "jane.doe@example.com" };
 
   return (
     <UserContext.Provider value={userData}>
