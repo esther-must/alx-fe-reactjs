034a3b3bd8157f459ad0474ee096a7050d33272b
esther-must <esther.must92@gmail.com>
2025-02-25 16:27:13 +0100

    enhancing recipe sharing app

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,7 +1,8 @@
+import { Link } from 'react-router-dom';
 import useRecipeStore from './recipeStore';
 
 const RecipeList = () => {
-    const recipes = useRecipeStore(state => state.recipes);
+    const recipes = useRecipeStore((state) => state.recipes);
 
     return (
       <div>
@@ -9,6 +10,7 @@ const RecipeList = () => {
           <div key={recipe.id}>
             <h3>{recipe.title}</h3>
             <p>{recipe.description}</p>
+            <Link to={`/recipes/${recipe.id}`}>View Details</Link>
           </div>
         ))}
       </div>
