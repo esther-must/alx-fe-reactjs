164001b265bb835abd4bc2cec1d8e7dbc667671d
esther-must <esther.must92@gmail.com>
2025-02-25 16:21:26 +0100

    Enhancing the Recipe Sharing Application with Detailed Recipe Management

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,13 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RecipeList from './components/RecipeList'
 import AddRecipeForm from './components/AddRecipeForm'
+import RecipeDetails from './components/RecipeDetails';
 
 function App() {
 
   return (
-    <>
-      <AddRecipeForm />
-      <RecipeList />
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={
+          <>
+            <AddRecipeForm />
+            <RecipeList />
+          </>
+        } />
+        <Route path='/recipe/:recipeId' element={<RecipeList />} />
+      </Routes>
+    </Router>
   )
 }
 
