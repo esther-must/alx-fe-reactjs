ba69e06e63e40e736aacd434537c337fbd15351d
esther-must <esther.must92@gmail.com>
2025-02-25 15:12:05 +0100

    Initiliase Recipe sharing app

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,4 +1,4 @@
-import useRecipeStore from './RecipeStore';
+import useRecipeStore from '../store/recipeStore';
 
 const RecipeList = () => {
     const recipes = useRecipeStore(state => state.recipes);
