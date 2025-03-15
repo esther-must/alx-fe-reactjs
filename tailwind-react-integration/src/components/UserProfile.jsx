a26db819e100afac01d3bb4da015443a667206ef
dainty92 <estheradexdainty@gmail.com>
2025-03-15 12:58:07 +0100

    responsive typography

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -6,7 +6,7 @@ function UserProfile() {
                 alt="User"
                 className="w-24 h-24 sm:w-36 sm:h-36 rounded-full mx-auto transition-transform duration-300 ease-in-out hover:scale-110"
             />
-            <h1 className="text-lg sm:text-xl text-blue-800 my-4 transition-colors duration-300 ease-in-out hover:text-blue-500">
+            <h1 className="text-lg md:text-xl text-blue-800 my-4 transition-colors duration-300 ease-in-out hover:text-blue-500">
                 Esther AJAYI
             </h1>
             <p className="text-sm sm:text-base text-gray-600">
