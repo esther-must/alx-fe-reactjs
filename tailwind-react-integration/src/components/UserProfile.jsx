0018b172811db3cc9d568286f61af90170b5beb5
dainty92 <estheradexdainty@gmail.com>
2025-03-15 13:01:16 +0100

    responsive image sizing

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -4,7 +4,7 @@ function UserProfile() {
             <img
                 src="https://esthercoders.netlify.app/images/profile-picture.png"
                 alt="User"
-                className="w-24 h-24 sm:w-36 sm:h-36 rounded-full mx-auto transition-transform duration-300 ease-in-out hover:scale-110"
+                className="w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 rounded-full mx-auto transition-transform duration-300 ease-in-out hover:scale-110"
             />
             <h1 className="text-lg md:text-xl text-blue-800 my-4 transition-colors duration-300 ease-in-out hover:text-blue-500">
                 Esther AJAYI
