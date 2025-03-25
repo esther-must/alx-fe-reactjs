e76827636603fe7770b5d5ec4a088f8fee69a8a6
dainty92 <estheradexdainty@gmail.com>
2025-03-25 11:11:03 +0100

    optimize code for performance

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -12,8 +12,6 @@ export const fetchUserData = async (username, location, minRepos) => {
 
     const response = await axios.get(`https://api.github.com/search/users?q=${queryString}`);
 
-    console.log("GitHub API Response:", response.data); // Debugging
-
     return response.data.items || []; // Ensure it's an array
   } catch (error) {
     console.error("Error fetching data:", error);
